72974de63be101a34a4378316dc97d266586a319
semsembella24-glitch <semsembella24@gmail.com>
2026-01-09 23:52:30 +0100

    Refactor Loan class to include date and enhance close
    Added date tracking for loan creation and improved close method.

--- src/🔗 loan.py
+++ src/🔗 loan.py
@@ -1,23 +1,31 @@
+from datetime import datetime
 
 class Loan:
-    def __init__(self, book, user):
-        self._book = book
+    def __init__(self, user, book):
         self._user = user
+        self._book = book
+        self._date = datetime.now()
         self._active = True
-
-    @property
-    def book(self):
-        return self._book
 
     @property
     def user(self):
         return self._user
 
     @property
+    def book(self):
+        return self._book
+
+    @property
+    def date(self):
+        return self._date
+
+    @property
     def active(self):
         return self._active
 
     def close(self):
-        self._active = False
-        self._book.return_book()
+        if self._active:
+            self._book.return_book()
+            self._active = False
+
 
